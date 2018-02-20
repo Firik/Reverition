e25d46ae11fd95fcd19b1779b9d15dbd858e41a9
firik <firik666go@gmail.com>
2018-02-20 18:34:05 +0300

    Rework standart project

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,13 +1,12 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import vue from "vue";
+import DragNDropComponent from './components/DragNDropComponent.vue';
 
-require('./bootstrap');
-
-window.Vue = require('vue');
+window.Vue = vue;
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +14,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('drag-n-drop-component', DragNDropComponent);
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/DragNDropComponent.vue
@@ -0,0 +1,34 @@
+<style>
+    .content {
+        height: 100vh;
+    }
+
+    .drag-zone {
+        border-radius: 2px;
+        border: 2px dashed #9c9c9c;
+        height: 50vh;
+        width: 30vw;
+        min-height: 2rem;
+        min-width: 10rem;
+    }
+
+    .drag-n-drop-text {
+        font-size: 3rem;
+        color: #c0c0c0;
+        cursor: default;
+    }
+</style>
+
+<template>
+    <div class="content d-flex justify-content-center align-items-center">
+        <div id="drag-zone" class="drag-zone d-flex justify-content-center align-items-center">
+            <span class="drag-n-drop-text">Drag-n-drop here</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
