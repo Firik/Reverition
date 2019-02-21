092c7d4ec9c243f7f841863da5eac41631164ec3
firik <vvkosty@gmail.com>
2019-02-21 15:18:37 +0300

    Remove package app.js

--- packages/vvkosty/reverition/src/resources/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import DragNDropComponent from './components/DragNDropComponent.vue';
-import resultPage from './components/resultPage';
-
-
-Vue.component('drag-n-drop-component', DragNDropComponent);
-Vue.component('result', resultPage);
-
-window.packageNameEvent = new Vue();
