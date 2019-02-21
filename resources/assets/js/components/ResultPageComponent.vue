5cbbccca2b17a95f3c1544fc3e7f38a77ec3b803
firik <vvkosty@gmail.com>
2019-02-21 17:11:13 +0300

    Add playable gif on result page

--- resources/assets/js/components/ResultPageComponent.vue
+++ resources/assets/js/components/ResultPageComponent.vue
@@ -1,4 +1,4 @@
-<style xmlns:v="http://www.w3.org/1999/xhtml">
+<style>
     .content {
         height: 100vh;
     }
@@ -10,7 +10,8 @@
             <h1>All done</h1>
         </div>
         <div class="flex-row">
-            <img src="example.com">
+            <img src="https://assets.hongkiat.com/uploads/on-click-animated-gif/gif-loaded.jpg" v-on:click="playGif"
+                 :data-alt="url">
         </div>
         <div class="flex-row">
             <input type="url" class="js-copytextarea" :value="url">
@@ -29,11 +30,24 @@
             return {
                 url: this.$store.state.url,
                 filename: this.$store.state.filename,
+
                 copyToBuffer() {
                     const copyTextarea = document.querySelector('.js-copytextarea');
                     copyTextarea.focus();
                     copyTextarea.select();
                     document.execCommand('copy');
+                },
+
+                playGif(event) {
+                    const gifImage = new Image();
+                    gifImage.src = event.target.getAttribute('data-alt');
+
+                    const img = event.target;
+                    const imgSrc = img.getAttribute('src');
+                    const imgAlt = gifImage.getAttribute('src');
+
+                    img.setAttribute('src', imgAlt);
+                    img.setAttribute('data-alt', imgSrc);
                 }
             }
         }
