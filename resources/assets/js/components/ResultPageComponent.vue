173f41e41e49e498bcadde57bc6893bc35c3f4f9
firik <vvkosty@gmail.com>
2019-02-21 21:19:34 +0300

    Add play button over preview gif

--- resources/assets/js/components/ResultPageComponent.vue
+++ resources/assets/js/components/ResultPageComponent.vue
@@ -9,6 +9,37 @@
     .copy-form {
         display: inline-flex;
     }
+
+    .player-block {
+        position: relative;
+    }
+
+    .button {
+        background: transparent;
+        box-sizing: border-box;
+        width: 0;
+        height: 74px;
+
+        border-color: transparent transparent transparent #383838;
+        transition: 100ms all ease;
+        cursor: pointer;
+        opacity: 0.85;
+
+        border-style: solid;
+        border-width: 37px 0 37px 60px;
+
+        position: absolute;
+        top: 65px;
+        left: 100px;
+    }
+
+    .button:hover {
+        border-color: transparent transparent transparent #444444;
+    }
+
+    button:focus {
+        outline: 0;
+    }
 </style>
 
 <template>
@@ -17,8 +48,9 @@
             <h1>All done!</h1>
         </div>
 
-        <div class="row">
-            <img :src="preview" v-on:click="playGif"
+        <div v-on:click="toPlayOrNotToPlay" class="row player-block">
+            <button class="button play-button"></button>
+            <img :src="preview"
                  :data-alt="url" class="img-rounded preview-gif">
         </div>
 
@@ -27,7 +59,7 @@
             <button v-on:click="copyToBuffer" type="button" class="btn btn-default">copy</button>
         </div>
 
-        <div class="row">or</div>
+        <span class="row">or</span>
 
         <div class="row">
             <a :href="url" :download="filename">Download?</a>
@@ -38,6 +70,31 @@
 <script>
     export default {
         data() {
+            let playButtonDisplay = '';
+
+            function togglePlayButton() {
+                const playButton = document.querySelector('.play-button');
+                const displayNone = 'none';
+                if (playButton.style.display === displayNone) {
+                    playButton.style.display = playButtonDisplay;
+                } else {
+                    playButtonDisplay = playButton.style.display;
+                    playButton.style.display = displayNone;
+                }
+            }
+
+            function toggleImages() {
+                const gifImage = new Image();
+                const img = document.querySelector('.preview-gif');
+                gifImage.src = img.getAttribute('data-alt');
+
+                const imgSrc = img.getAttribute('src');
+                const imgAlt = gifImage.getAttribute('src');
+
+                img.setAttribute('src', imgAlt);
+                img.setAttribute('data-alt', imgSrc);
+            }
+
             return {
                 url: this.$store.state.url,
                 filename: this.$store.state.filename,
@@ -50,16 +107,9 @@
                     document.execCommand('copy');
                 },
 
-                playGif(event) {
-                    const gifImage = new Image();
-                    gifImage.src = event.target.getAttribute('data-alt');
-
-                    const img = event.target;
-                    const imgSrc = img.getAttribute('src');
-                    const imgAlt = gifImage.getAttribute('src');
-
-                    img.setAttribute('src', imgAlt);
-                    img.setAttribute('data-alt', imgSrc);
+                toPlayOrNotToPlay() {
+                    togglePlayButton();
+                    toggleImages();
                 }
             }
         }
