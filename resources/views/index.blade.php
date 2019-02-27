dcc9e96691a0952e62ac304c9cc8e7946eb59c80
firik <vvkosty@gmail.com>
2019-02-27 10:44:55 +0300

    Add meta description

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#f5f8fa" />
+    <meta name="Description" content="Create a reverse gif animation in a few seconds. Just download the gif and get the result.">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" rel="script" defer></script>
