7511346543c3343d2500842df00df5af21604959
firik <vvkosty@gmail.com>
2019-02-27 10:46:38 +0300

    Change title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,12 +11,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#f5f8fa" />
-    <meta name="Description" content="Create a reverse gif animation in a few seconds. Just download the gif and get the result.">
+    <meta name="Description" content="Create a revert gif animation in a few seconds. Just download the gif and get the result.">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" rel="script" defer></script>
 
-    <title>{{ config('app.name')  }}</title>
+    <title>{{ config('app.name')  }} - create reverted gif online</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
